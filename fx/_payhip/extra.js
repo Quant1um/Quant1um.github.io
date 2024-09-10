b3e2e3e7a032b6002f614787c27e9161513563b2
Quant1um <spitfirexv22@gmail.com>
2024-09-10 16:52:29 +0300

    fx.amee.ee: payhip extra styling stuff

---
+++ fx/_payhip/extra.js
@@ -0,0 +1,22 @@
+[...document.querySelectorAll(".product-card-wrapper")].forEach((card) => {
+    let image = card.querySelector(".card__inner");
+    let bounds;
+    card.addEventListener("mouseleave", () => image.style.transform = "");
+    card.addEventListener("mouseenter", () => bounds = image.getBoundingClientRect());
+    card.addEventListener("mousemove", (e) => {
+        const x = e.clientX - bounds.x - bounds.width / 2;
+        const y = e.clientY - bounds.y - bounds.height / 2;
+        const d = Math.hypot(x, y) + 1;
+
+        image.style.transform = `
+        scale3d(1.03, 1.03, 1.03)
+        perspective(1200px) 
+        rotate3d(
+          ${-y / 200},
+          ${x / 200},
+          0,
+          ${Math.log(d) * 2}deg
+        )
+      `;
+    });
+});
