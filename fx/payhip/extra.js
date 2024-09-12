04ccd11601b154bab54ce5840a646371cbc8e84a
Quant1um <spitfirexv22@gmail.com>
2024-09-13 00:21:55 +0300

    rollback the last one

--- fx/payhip/extra.js
+++ fx/payhip/extra.js
@@ -1,7 +1,5 @@
 [...document.querySelectorAll(".product-card-wrapper")].forEach((card) => {
-    const image = card.querySelector(".card__inner");
-    const glow = card.querySelector(".card__content");
-
+    let image = card.querySelector(".card__inner");
     let bounds;
     card.addEventListener("mouseleave", () => image.style.transform = "");
     card.addEventListener("mouseenter", () => bounds = image.getBoundingClientRect());
@@ -11,24 +9,14 @@
         const d = Math.hypot(x, y) + 1;
 
         image.style.transform = `
-            scale3d(1.03, 1.03, 1.03)
-            perspective(1200px) 
-            rotate3d(
-                ${-y / 200},
-                ${x / 200},
-                0,
-                ${Math.log(d) * 2}deg
-            )
-        `;
-
-        glow.style.backgroundImage = `
-            radial-gradient(
-            circle at
-            ${center.x * 2 + bounds.width / 2}px
-            ${center.y * 2 + bounds.height / 2}px,
-            #ffffff55,
-            #0000000f
-            )
+          scale3d(1.03, 1.03, 1.03)
+          perspective(1200px) 
+          rotate3d(
+            ${-y / 200},
+            ${x / 200},
+            0,
+            ${Math.log(d) * 2}deg
+          )
         `;
     });
 });
